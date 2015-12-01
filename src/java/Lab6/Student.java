45f22cff2b6476d18dc8df2d327f87d67d6567fa
daiker0330 <daiker0330@gmail.com>
2015-12-01 15:05:07 -0800

    add enrolling

--- src/java/Lab6/Student.java
+++ src/java/Lab6/Student.java
@@ -10,23 +10,34 @@ package Lab6;
  * @author yunfan
  */
 public class Student extends AnyParty {
+	private final int MaxUnits =15;
+	private int StudentUnits;
+	private Enrolling enroll;
+	
 
-    private final int MaxUnits = 15;
-    private int StudentUnits;
 
-    public int getMaxUnits() {
-        return MaxUnits;
-    }
-
-    public int getStudentUnits() {
-        return StudentUnits;
-    }
-
-    public String getUserName() {
+	
+    public String getUserName(){
         return super.getUserName();
     }
-
-    public String getPsd() {
+    public  String getPsd(){
         return super.getPassword();
+    }
+    
+    public int getMaxUnits(){
+    	return MaxUnits;
+    }
+    public int getStudentUnits(){
+    	return StudentUnits;
+    }
+    public void setStudentUnits(int StudentUnits){
+    	this.StudentUnits = StudentUnits;
+    }
+    public String CheckUnits(){
+    	if (getMaxUnits() <= getStudentUnits()){
+    		return "Can't add anymore";
+    	}
+    	else
+    		return "You have "+getStudentUnits()+" units left for fulltime";
     }
 }
