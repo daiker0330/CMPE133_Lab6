8e5070d01c55c9ddf626853327c46f0915a710f7
Karma Pandya <kay.pandya@gmail.com>
2015-12-01 17:03:46 -0800

    someCHanges
    someChanges

---
+++ src/java/Lab6/Schedule.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab6;
+
+/**
+ *
+ * @author kay.pandya
+ */
+public class Schedule extends AnyForm{
+    private Student std;
+    
+    public void setStudent(Student std){
+        this.std = std;
+    }
+    
+    public String print(){
+        std.getEnroll().getCourseList();
+    }
+}
